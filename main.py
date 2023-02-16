53303e54f8e750e4bcdf9b649e9cf1d42177988e
T2fastpage <12james15545@gmail.com>
2023-02-15 18:26:02 -0800

    test

--- main.py
+++ main.py
@@ -5,19 +5,17 @@ from flask import render_template  # import render_template from "public" flask 
 
 # import "packages" from "this" project
 from __init__ import app  # Definitions initialization
-from model.jokes import initJokes
-from model.users import initUsers
+from model.users import initActivity
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
+
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
@@ -37,8 +35,7 @@ def stub():
 
 @app.before_first_request
 def activate_job():
-    initJokes()
-    initUsers()
+    initActivity()
 
 # this runs the application on the development server
 if __name__ == "__main__":
