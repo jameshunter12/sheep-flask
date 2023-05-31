86851c854046705e84a4d5060472e91d57316f3b
T2fastpage <12james15545@gmail.com>
2023-05-30 23:07:49 -0700

    update everything

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
-import threading
+from flask_cors import CORS
+from sheep_api import app, db
 
-# import "packages" from flask
-from flask import render_template  # import render_template from "public" flask libraries
+from sheep_api.api.nhl import player_bp
+from sheep_api.api.user import user_bp
 
-# import "packages" from "this" project
-from __init__ import app  # Definitions initialization
-from model.users import initUsers
+from sheep_api.model.player import init_players
+from sheep_api.model.username import init_users
 
-# setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
+app.register_blueprint(player_bp)
+app.register_blueprint(user_bp)
 
-# setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
-
-# register URIs
-
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
-app.register_blueprint(app_projects) # register app pages
-
-@app.errorhandler(404)  # catch for URL not found
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
-
-@app.route('/')  # connects default URL to index() function
-def index():
-    return render_template("index.html")
-
-@app.route('/stub/')  # connects /stub/ URL to stub() function
-def stub():
-    return render_template("stub.html")
 
 @app.before_first_request
-def activate_job():
-    initUsers()
+def init_db():
+    with app.app_context():
+        db.create_all()
+        init_players()
+        init_users()
 
-# this runs the application on the development server
 if __name__ == "__main__":
-    # change name for testing
-    from flask_cors import CORS
     cors = CORS(app)
-    app.run(debug=True, host="0.0.0.0", port="8086")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./volumes/sqlite.db"
+    app.run(debug=True, host="0.0.0.0", port="8067")
